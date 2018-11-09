340c24f2320fc7f9c7bb366e8d449e106be9526d
BM <benji.matta@gmail.com>
2018-11-09 02:55:56 -0600

    Created function (bpstatus4th) that classifies patients into BP status categories based on 4th report (revised in 2004) guidelines.

--- BPstatus4th.R
+++ BPstatus4th.R
@@ -1,4 +1,4 @@
-# function to categorize patient's clinic BP based on the 4th report (1996) guidelines:
+# function to categorize patient's clinic BP based on the 4th report (1996,revised in 2004) guidelines:
 # input: clinic SBP and DBP, age (yrs), gender (male=1, female=0), height (cm)
 # for reference, see Table 3 in PDF at the following URL: http://pediatrics.aappublications.org/content/114/Supplement_2/555#ref-1
 
@@ -8,4 +8,16 @@
 # 2=Stage 1 HTN
 # 3=Stage 2 HTN
 
+# bpp4<- function(age,heightp,gender,sysdia,bp=NULL,percentile=NULL)
 
+bpstatus4th<-function(sbp,dbp,age,gender,heightp) {
+  sbpp4<-bpp4(age,heightp,gender,1,sbp)
+  dbpp4<-bpp4(age,heightp,gender,2,dbp)
+  if (all(sbpp4<90,dbpp4<90)) return (0)
+  sbpp<-sapply(c(90,95,99),function(x) bpp4(age,heightp,gender,1,percentile=x))
+  dbpp<-sapply(c(90,95,99),function(x) bpp4(age,heightp,gender,2,percentile=x))
+  if (any(sbp>sbpp[3]+5,dbp>dbpp[3]+5,sbp>=160,dbp>=100)) return(3)
+  if (any(between(sbp,sbpp[2],sbpp[3]+5),between(dbp,dbpp[2],dbpp[3]+5),between(sbp,140,159.9),between(dbp,90,99.9))) return(2)
+  return(1)
+  
+}
--- bpp4.R
+++ bpp4.R
@@ -2,7 +2,7 @@
 # reference: http://pediatrics.aappublications.org/content/114/Supplement_2/555
 # inputs:
 #    - age (yrs)
-#    - height z-score
+#    - height percentile
 #    - gender (male=1, female=0)
 #    - sysdia (1 for systolic, 2 for diastolic)
 #    - bp (either sbp or dbp) - if inverse function is used, the percentile argument must be set (see below)
@@ -14,17 +14,17 @@ load("bp4th_coefs.RData")
 # convert height to inches and take only coefficients needed for computation
 #height<-height/2.54
 
-bpp4<- function(age,heightz,gender,sysdia,bp=NULL,percentile=NULL) {
+bpp4<- function(age,heightp,gender,sysdia,bp=NULL,percentile=NULL) {
   # check that all parameters are provided and are valid
-  if (any(!between(age,1,18),anyNA(c(bp,age,gender,heightz)))) return(NA)
+  if (any(!between(age,1,18),anyNA(c(bp,age,gender,heightp)))) return(NA)
   # extract only the relevant coefficients based on gender and sysdia
   bp4th_coefs.1<-bp4th_coefs[c(2,4)-gender]
   bp4th_coefs.1<-bp4th_coefs.1[c(sysdia)]
   # calculation of average BP (mu) is based on reference above (appendix B)
   first<-function(x,age) bp4th_coefs.1[1+x,1]*(age-10)^x
-  second<-function(x,heightz) bp4th_coefs.1[x+5,1]*heightz^x
+  second<-function(x,heightp) bp4th_coefs.1[x+5,1]*heightp^x
   sum1<-sum(unlist(sapply(1:4,function(x) first(x,age))))
-  sum2<-sum(unlist(sapply(1:4,function(x) second(x,qnorm(heightz/100)))))
+  sum2<-sum(unlist(sapply(1:4,function(x) second(x,qnorm(heightp/100)))))
   mu<-unlist(bp4th_coefs.1[1,1]+sum1+sum2)
   if (!is.null(percentile)) {
     bpp4<-qnorm(percentile/100)*unlist(bp4th_coefs.1[10,1])+mu}
